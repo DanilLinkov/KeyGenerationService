bd7b5f7676f88204bc3d3f63aabb058a5e0603a8
DanilLinkov <danil.linkov@gmail.com>
2022-01-31 15:55:43 +1300

    Changing builder to add appsettings

--- Startup.cs
+++ Startup.cs
@@ -32,9 +32,14 @@ namespace KeyGenerationService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
